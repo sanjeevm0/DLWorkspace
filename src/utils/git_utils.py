ad81688640f0ef41f2064126eaf1cb63af4b4371
Sanjeev Mehrotra <sanjeevm0@hotmail.com>
2017-04-28 11:16:49 -0700

    Fix

--- src/utils/git_utils.py
+++ src/utils/git_utils.py
@@ -5,9 +5,9 @@ import yaml
 
 def github_hash(repo, branch) :
     curlCmd = ['curl', "https://api.github.com/repos/" + repo + "/branches/" + branch]
-    #print "Command: " + ' '.join(curlCmd)
+    print "Command: " + ' '.join(curlCmd)
     output = subprocess.check_output(curlCmd)
-    #print "Output: " + output
+    print "Output: " + output
     ret = yaml.load(output)
     #print ret
     sha = ret["commit"]["sha"]
