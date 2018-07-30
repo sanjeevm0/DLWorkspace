f1a0cf6e14bbaf380736c910e48a323a3058cc07
Sanjeev Mehrotra <sanjeevm0@hotmail.com>
2018-07-30 15:58:24 -0700

    simple test

---
+++ src/ARM/simpletest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+sudo apt-get update 
+sudo apt-get install -y --no-install-recommends \
+        apt-utils \
+        software-properties-common \
+        git \
+        curl \
+        python-pip \
+        wget \
+        cpio \
+        mkisofs \
+        apt-transport-https \
+        openssh-client \
+        ca-certificates \
+        sshpass
