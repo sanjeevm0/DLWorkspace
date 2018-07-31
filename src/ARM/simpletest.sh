d5535658929721fbeab73c3eb4db2389a969fc86
Sanjeev Mehrotra <sanjeevm0@hotmail.com>
2018-07-31 15:17:46 -0700

    update

--- src/ARM/simpletest.sh
+++ src/ARM/simpletest.sh
@@ -3,4 +3,4 @@
 echo $@ > /home/dlwsadmin/shellargs.txt
 cd /home/dlwsadmin/dlworkspace/src/ClusterBootstrap
 # Copy ssh keys
-../ARM/createconfig.py sshkey $@
+../ARM/createconfig.py sshkey $@ > /home/dlwsadmin/sshkeycopy.txt
