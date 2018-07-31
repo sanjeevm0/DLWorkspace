5646a4dae0147b574f5d70de5968229058200743
Sanjeev Mehrotra <sanjeevm0@hotmail.com>
2018-07-31 14:53:32 -0700

    update

--- src/ARM/simpletest.sh
+++ src/ARM/simpletest.sh
@@ -1,18 +1,6 @@
 #!/bin/bash 
 
-sudo apt-get update 
-sudo apt-get install -y --no-install-recommends \
-        apt-utils \
-        software-properties-common \
-        git \
-        curl \
-        python-pip \
-        wget \
-        cpio \
-        mkisofs \
-        apt-transport-https \
-        openssh-client \
-        ca-certificates \
-        sshpass
-
-git clone http://github.com/Microsoft/DLWorkspace /home/dlwsadmin/dlworkspace
+echo $@ > /home/dlwsadmin/shellargs.txt
+cd /home/dlwsadmin/dlworkspace/src/ClusterBootstrap
+# Copy ssh keys
+../ARM/createconfig.py sshkey $@
